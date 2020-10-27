3cb9c48b728357b249438631831b68e57328c985
施航程(Arvin) <arvin2@sfmail.sf-express.com>
2020-10-27 11:44:40 +0800

    版本二，对实体类继承的的父类和接口无要求

--- src/main/java/com/arvinclub/orderhelper/demo/Demo.java
+++ src/main/java/com/arvinclub/orderhelper/demo/Demo.java
@@ -1,24 +1,21 @@
 /*
  * Copyright (C), 2020, 顺丰科技
- * FileName: Main
+ * FileName: Demo
  * Author: 01397386
- * Date: 2020/10/26 11:18
+ * Date: 2020/10/27 11:10
  * Description:
  */
 package com.arvinclub.orderhelper.demo;
+
 
 import com.arvinclub.orderhelper.OrderUtils;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
-public class Main {
+public class Demo {
     public static void main(String[] args) {
         List<Student> studentList = new ArrayList<Student>(){{
-//            for (int i = 0; i < 10000; i++) {
-//                add(new Student("施航程", new Random().nextInt(100)));
-//            }
             add(new Student(456,"施航程", 22));
             add(new Student(1234,"施航程", 54));
             add(new Student(34,"施航程", 12));
